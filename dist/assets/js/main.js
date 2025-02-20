2a4bc1115d5647d20389edfae0445fa1d0e8c451
Дмитрий <1primo1@mail.ru>
2025-02-20 21:06:24 +0300

    Отрасли внутр

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -15696,6 +15696,72 @@ const ourOfficeSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["default"
 
 });
 
+const previewExpertSlider = new swiper_bundle__WEBPACK_IMPORTED_MODULE_0__["default"]('.preview-experts__slider', {
+    // Стрелки
+    navigation: {
+        nextEl: '.preview-experts__btn-slider-next',
+        prevEl: '.preview-experts__btn-slider-prev',
+    },
+
+    /*Отступ у карточек*/
+    spaceBetween: 12,
+    /*Показывать по n карточек*/
+    slidesPerView: 1.1,
+
+    // effect: "coverflow",
+    // coverflowEffect: {
+    //     rotate: 0,              // added (Rotate of the prev and next slides)
+    //     // depth: 100,             // added (Depth of the prev and next slides)
+    //     // stretch: 100,            // added (Space between the slides)
+    //     modifier: 0.2,            // added (Multiply the values of rotate, depth, and stretch)
+    //     scale: 0.6,               // added (Size ratio of the prev and next slides)
+    //     slideShadows: false, 
+    //  },
+
+    /* При достижении конца, перепрыгнуть в начало */
+    // rewind: true,
+    /*Увеличение при наведении курсора мыши */
+    // zoom: true,
+    /*Ленивая подгрузка */
+    lazy: true,
+    /*Бесконечная прокрутка */
+    // loop: true,
+    /*Ориентация */
+    // direction: 'vertical',
+    /*Авто высота*/
+    // autoHeight: true,
+    /*иконка захвата при наведении на слайд*/
+    grabCursor: true,
+    /*Автоматическое перелистывание*/
+    // autoplay: {
+    //     delay: 5000,
+    //   },
+    /*Брек-поинты*/
+    breakpoints: {
+        1400: {
+            slidesPerView: 4.8,
+            spaceBetween: 16,
+        },
+        1300: {
+            slidesPerView: 3.8,
+            spaceBetween: 15,
+        },
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 2.8,
+            spaceBetween: 15,
+        },
+        575: {
+            slidesPerView: 1.5,
+            spaceBetween: 12,
+        }
+    },
+
+});
+
 
 
 
